8fa3ae506b06b60b7baa5abaf5cfd31dd7100774
Max Podkorytov <4273004+tenpercent@users.noreply.github.com>
2025-08-05 16:23:21 -0500

    boilerplate to run a test

---
+++ test/ck_tile/tile_distribution/test_tile_distribution.cpp
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) Advanced Micro Devices, Inc. All rights reserved.
+
+#include <gtest/gtest.h>
+#include "ck_tile/host.hpp"
+#include "ck_tile/core.hpp"
+#include "ck_tile/host/kernel_launch.hpp"
+
+
+class TestTileDistribution: public ::testing::Test
+{
+    protected:
+    void Run()
+    {
+
+    }
+};
+
+TEST_F(TestTileDistribution, Dummy)
+{
+    Run();
+}
